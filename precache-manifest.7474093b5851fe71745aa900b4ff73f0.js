c62108871f114f8fddee9bcff066381af4410d0b
Phakphoom Khumpuan <6303013002@feu.edu>
2020-07-27 22:29:48 +0700

    Updates

---
+++ precache-manifest.7474093b5851fe71745aa900b4ff73f0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5812495afe7e3c5764b384322c50e8d4",
+    "url": "/cmtelection-frontend.io/index.html"
+  },
+  {
+    "revision": "9f77fa2a5805fef4d4aa",
+    "url": "/cmtelection-frontend.io/static/css/2.6619e73d.chunk.css"
+  },
+  {
+    "revision": "ffdfb7058d6db1c68e56",
+    "url": "/cmtelection-frontend.io/static/css/main.b3229d83.chunk.css"
+  },
+  {
+    "revision": "9f77fa2a5805fef4d4aa",
+    "url": "/cmtelection-frontend.io/static/js/2.784c95d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cmtelection-frontend.io/static/js/2.784c95d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffdfb7058d6db1c68e56",
+    "url": "/cmtelection-frontend.io/static/js/main.3f0b8bd1.chunk.js"
+  },
+  {
+    "revision": "323b4aa04c92e123b824",
+    "url": "/cmtelection-frontend.io/static/js/runtime-main.69f6b880.js"
+  },
+  {
+    "revision": "9d3bf71bfccf0a91310ea03543314608",
+    "url": "/cmtelection-frontend.io/static/media/Party01.9d3bf71b.jpg"
+  },
+  {
+    "revision": "902f5dd1a77bc307c5c54635ac80b0e6",
+    "url": "/cmtelection-frontend.io/static/media/Party02.902f5dd1.jpg"
+  },
+  {
+    "revision": "f3f56632319542480f38c5d16edb4684",
+    "url": "/cmtelection-frontend.io/static/media/Party03.f3f56632.jpg"
+  },
+  {
+    "revision": "79201a49fce54cab19f6c3c472b39491",
+    "url": "/cmtelection-frontend.io/static/media/Party04.79201a49.jpg"
+  },
+  {
+    "revision": "66b15a43635eff5686255f19de5755c0",
+    "url": "/cmtelection-frontend.io/static/media/Party05.66b15a43.jpg"
+  },
+  {
+    "revision": "5fff8cc03f502fbd678e8cc3bc398dd8",
+    "url": "/cmtelection-frontend.io/static/media/Party06.5fff8cc0.jpg"
+  },
+  {
+    "revision": "03f5225fe79fbcacea3a6e1371bf494a",
+    "url": "/cmtelection-frontend.io/static/media/Party07.03f5225f.jpg"
+  },
+  {
+    "revision": "63b80b3ce030ace9f5a7d06291f44272",
+    "url": "/cmtelection-frontend.io/static/media/cmt-logo-whiteRoundBG-01.63b80b3c.png"
+  }
+]);
